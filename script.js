add2db30e6efabe7fb3c3619872bb0ea27f58efe
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-05-30 11:47:38 -0400

    Create if and else condition to output choice

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
 function getComputerChoice() {
     let randomizer = Math.floor(Math.random() * 3) + 1;
 
+    if (randomizer = 1) {
+        return console.log("rock");
+    } else if (randomizer = 2) {
+       return console.log("paper");
+    } else {
+       return console.log("scissors");
+    }
+
 }

5be917dcc7e3d7150857f0336cd0c1c32d351b37
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-06-01 15:55:36 -0400

    Add humanScore and computerScore to global scope

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 //  Returns following string values for computer : rock, paper, scissors
 function getComputerChoice() {
     let randomizer = Math.floor(Math.random() * 3) + 1;

645568fec75cd32811d5f8f8781989a53cc371c3
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-06-02 11:45:40 -0400

    Add else if condition
    Human choice being 'rock' and computer choice being 'scissors' using AND.

--- script.js
+++ script.js
@@ -25,5 +25,7 @@ function playRound(humanChoice, computerChoice) {
     if(humanChoice.toLowerCase() === "rock" && computerChoice === "paper") {
         alert("You lose! Paper beats Rock.")
         return console.log(`Computer Score : ${++computerScore}`);
+    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+        
     }
 }

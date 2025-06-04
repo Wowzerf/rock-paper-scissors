342806a809b964897c41f0fb3e39271472147bd6
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-06-04 12:09:31 -0400

    Delete parentheses
    Caused an error by making 'scissors' a function.

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function playRound(humanChoice, computerChoice) {
         } else {
             alert("It's a draw!");
         }
-   } else if(humanChoice.toLowercase === "scissors"()) {
+   } else if(humanChoice.toLowercase === "scissors") {
     if(computerChoice === "rock") {
         alert("You Lose! Rock beats Paper.");
         return console.log(`Computer Score : ${++computerScore}`);

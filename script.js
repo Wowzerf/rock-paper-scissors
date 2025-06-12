6b0d7e2e5f7a2208fb9b31ccb40f56e6326b0179
Wowzerf <79344383+Wowzerf@users.noreply.github.com>
2025-06-12 12:18:23 -0400

    Console log 'Final Score'

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ function playGame() {
         console.log(`Computer Score : ${computerScore}`);
         console.log("You win the match!");
     } else if(computerScore > humanScore) {
+        console.log("Final Score");
         console.log("You lose the match");
     } 
 }
